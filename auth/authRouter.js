6be0ba9a5521145e00aeef019843304ca6b56a7a
fuston05 <fuston05@yahoo.com>
2020-03-30 11:26:02 -0400

    added a check to see if username or email already exists when registering a new user

--- auth/authRouter.js
+++ auth/authRouter.js
@@ -2,25 +2,43 @@ const users = require('../data/users-model');
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
-const generateToken= require('../utils/token');
-
+const generateToken = require('../utils/token');
 
 // /add/register a new user
 router.post('/register', (req, res) => {
   const newUserInfo = req.body;
   const password = req.body.password;
 
+  const alreadyExists = (userInfo) => {
+    const username = userInfo.username;
+    users.findBy({ username })
+      .then(user => {
+        if (user && user.email === userInfo.email) {
+          console.log('user found')
+          return true;
+        } else {
+          console.log('user not found')
+          return false;
+        }
+      })
+      .catch(error => {
+        console.log('error', error)
+      })
+  }//end alreadyExists
+
   //validation
-  if (!newUserInfo.username) {
-    res.status(400).json({ message: 'Missing required field: username' })
+  if (alreadyExists(newUserInfo)===true ) {
+    res.status(400).json({ message: 'That username already exists' });
+  } else if (!newUserInfo.username) {
+    res.status(400).json({ message: 'Missing required field: username' });
   } else if (!newUserInfo.first_name || !newUserInfo.last_name) {
-    res.status(400).json({ message: 'Missing required field(s): first name, last name' })
+    res.status(400).json({ message: 'Missing required field(s): first name, last name' });
   } else if (!newUserInfo.email) {
-    res.status(400).json({ message: 'Missing required field: email' })
+    res.status(400).json({ message: 'Missing required field: email' });
   } else if (!newUserInfo.password) {
-    res.status(400).json({ message: 'Missing required field: password' })
+    res.status(400).json({ message: 'Missing required field: password' });
   } else if (!newUserInfo.role) {
-    res.status(400).json({ message: 'Missing required field: role' })
+    res.status(400).json({ message: 'Missing required field: role' });
   } else {// all is valid
 
     const rounds = process.env.ROUNDS || 8;
@@ -44,24 +62,24 @@ router.post('/register', (req, res) => {
 
 //login, returns welcome w/ username, and token
 router.post('/login', (req, res) => {
-  const {username, password}= req.body;
+  const { username, password } = req.body;
   //verify
-  users.findBy({username})
-  .then(user => {
-    if(user &&
-      bcrypt.compareSync(password, user.password)
-      ){
+  users.findBy({ username })
+    .then(user => {
+      if (user &&
+        bcrypt.compareSync(password, user.password)
+      ) {
         //set/send a token
-        const token= generateToken(user);
+        const token = generateToken(user);
 
-      res.status(200).json({message: `Welcome, ${user.username}`, token});
-    }else{
-      res.status(401).json({error: "Invalid credentials"});
-    }
-  })
-  .catch((error) => {
-    res.status(500).json({error: "Could not process your request"});
-  })
+        res.status(200).json({ message: `Welcome, ${user.username}`, token });
+      } else {
+        res.status(401).json({ error: "Invalid credentials" });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({ error: "Could not process your request" });
+    })
 });// end login
 
 module.exports = router;
