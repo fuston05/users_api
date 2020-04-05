9534b509b4b2aa0275a77aa26d5e1a4c03b6a451
fuston05 <fuston05@yahoo.com>
2020-04-05 14:20:50 -0400

    changed knex file prodection env to use postgresql, set up all env variables for db access etc

--- knexfile.js
+++ knexfile.js
@@ -51,17 +51,20 @@ module.exports = {
   production: {
     client: 'pg',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      database: process.env.DB_URL,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS
     },
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: './database/migrations'
+    },
+    seeds: {
+      directory: './database/seeds'
+    },
   }
 
 };
