340ab941b211c438441b51697db0be84470aa6e5
fuston05 <fuston05@yahoo.com>
2020-05-20 21:00:16 -0400

    deletes all files so I can start over for practice

--- index.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-const server= require('./api/server');
-const port= process.env.PORT || 5000;
-
-server.listen(port, () => {
-  console.log(`\n ** Listening on port: ${port} ** \n`);
-});
