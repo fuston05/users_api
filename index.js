44694782326f55f30aaef1bb2287282e7dcd1eee
scott fuston <fuston05@yahoo.com>
2020-12-11 22:29:41 -0500

    edits PORT env usage

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // main index
-require('dotenv').config();
-const server = require('./api/server');
-const PORT = process.env.PORT; 
+require("dotenv").config();
+const server = require("./api/server");
+const PORT = process.env.PORT || 5001;
 
 server.listen(PORT, () => {
   console.log(`**** Server listening on port: ${PORT} ****`);
