ec9099684848f1088dd41a931de891bdc85b1c6e
scott fuston <fuston05@yahoo.com>
2020-12-20 12:21:08 -0500

    restrict midware now checks against the user role found in the jwt

--- data/middleware/restrict.js
+++ data/middleware/restrict.js
@@ -6,8 +6,11 @@ const restrict = (req, res, next) => {
   const token = req.headers.authorization;
   const sec = process.env.JWT_SECRET;
   const decoded = jwt.verify(token, sec);
-  console.log('decoded', decoded);
-  next();
+  if (decoded.role !== 2) {
+    res.status(401).json({Error: "Not authorized"})
+  } else {
+    next();
+  }
 };
 
 module.exports = {
