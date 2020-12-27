73857c07904a268955a31deb698f449af7388364
scott fuston <fuston05@yahoo.com>
2020-12-27 10:53:23 -0500

    adds morgan logs to access.log file

--- api/server.js
+++ api/server.js
@@ -5,6 +5,8 @@ const server = express();
 const helmet = require("helmet");
 const morgan = require('morgan');
 const { assignId } = require('../logs/morgan_tokens');
+const fs = require('fs');
+const path = require('path');
 
 // global middleware
 server.use(helmet());
@@ -12,8 +14,12 @@ server.use(cors());
 server.use(express.json());
 // assignId is used in the morgan token 'id'
 server.use(assignId);
+
+// writes morgan logs to file
+let accessLogStream = fs.createWriteStream(path.join(__dirname, '../logs/access.log'), { flags: 'a' });
 // logger
 server.use(morgan('id: :id, method: :method, date: :date(iso), remoteAddr: :remote-addr, url: :url, status: :status, userAgent: :user-agent, resTime: :response-time'));
+server.use(morgan('id: :id, method: :method, date: :date(iso), remoteAddr: :remote-addr, url: :url, status: :status, userAgent: :user-agent, resTime: :response-time \n\n', {stream: accessLogStream}));
 
 // define routers
 const userRouter = require("../routers/userRouter");
