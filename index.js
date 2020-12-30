8dd908be36e0a913b0c084a23c1eb57d8ca80f06
scott fuston <fuston05@yahoo.com>
2020-12-30 15:22:09 -0500

    removed dotenv from dependencies, added it to dev dependencies

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 require("dotenv").config();
 const server = require("./api/server");
 const PORT = process.env.PORT || 5001;
-console.log("env: ", process.env.NODE_ENV);
 
 server.listen(PORT, () => {
   console.log(`**** Server listening on port: ${PORT} ****`);
