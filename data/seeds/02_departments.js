acd752bd38c0a423b7ab588460e1813e81d45fc6
scott fuston <fuston05@yahoo.com>
2021-05-11 23:12:06 -0400

    fixes typo in departments seed file.

--- data/seeds/02_departments.js
+++ data/seeds/02_departments.js
@@ -3,39 +3,39 @@ exports.seed = function (knex) {
   return knex("departments").insert([
     {
       department: "Product Management",
-      description: "Manages products at all levels"
+      description: "Manages products at all levels",
     },
     {
       department: "Business Development",
-      description: "Business Development department description goes here."
+      description: "Business Development department description goes here.",
     },
     {
       department: "Accounting",
-      description: "Accounting department description goes here.stin"
+      description: "Accounting department description goes here.",
     },
     {
       department: "Production",
-      description: "Production department description goes here."
+      description: "Production department description goes here.",
     },
     {
       department: "Services",
-      description: "Services department description goes here."
+      description: "Services department description goes here.",
     },
     {
       department: "Sales",
-      description: "Sales department description goes here."
+      description: "Sales department description goes here.",
     },
     {
       department: "Support",
-      description: "Support department description goes here."
+      description: "Support department description goes here.",
     },
     {
       department: "Research and Development",
-      description: "Research and Development department description goes here."
+      description: "Research and Development department description goes here.",
     },
     {
       department: "Training",
-      description: "Training department description goes here."
+      description: "Training department description goes here.",
     },
   ]);
 };
